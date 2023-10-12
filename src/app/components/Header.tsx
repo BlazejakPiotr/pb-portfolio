55d5f9733432c7a93e8608fcb9bdaabf6b80e9b7
@piotr.blaz <piotr.blazejak@yahoo.com>
2023-10-12 19:38:37 +0200

    feat(header): added image & text

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 type Props = {};
@@ -6,7 +7,18 @@ const Header = (props: Props) => {
   return (
     <section className="h-[calc(100vh-56px)] w-full flex justify-center items-center">
       <div className="flex flex-col items-center justify-center space-y-4">
-        Header
+        <div className=" bg-light-navy border-light-blue border-2 rounded-full flex items-center justify-center shadow-md shadow-gray-400 hover:scale-105 hover:border-blue hover:bg-orange duration-500 p-2">
+          <Image
+            src="/me_pic.webp"
+            alt="picture of me"
+            className="rounded-full"
+            height={150}
+            width={150}
+          />
+        </div>
+        <p className="text-blue font-bold text-2xl">
+          Welcome, my name is <span className="text-orange">Piotr</span>
+        </p>
       </div>
     </section>
   );
