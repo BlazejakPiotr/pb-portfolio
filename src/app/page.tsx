ba10ebae170aed3c422248d2685f748c29d52075
@piotr.blaz <piotr.blazejak@yahoo.com>
2023-10-12 18:18:56 +0200

    fix(home): removing not existing component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Navbar from "./components/Navbar";
-
 export default function Home() {
   return (
     <div>
       {/* navbar */}
-      <Navbar />
       {/* header */}
       {/* about */}
       {/* skills */}
