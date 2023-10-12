a7660c51bead83e6ba9ae3136c9cfe4848b56fa4
@piotr.blaz <piotr.blazejak@yahoo.com>
2023-10-12 18:13:23 +0200

    feat(nav): basic component created

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <nav className="max-w-7xl mx-auto p-5 flex items-center justify-between">
+      <div></div>
+      <div></div>
+    </nav>
+  );
+};
+
+export default Navbar;
