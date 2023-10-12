4c3c6cfa060bbc2e3c3f623599153f9a3ebf2f86
@piotr.blaz <piotr.blazejak@yahoo.com>
2023-10-12 19:01:47 +0200

    feat(nav): navlinks setup

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
@@ -6,7 +7,32 @@ const Navbar = (props: Props) => {
   return (
     <nav className="max-w-7xl mx-auto p-5 flex items-center justify-between">
       <div></div>
-      <div></div>
+      <div className="flex items-center space-x-4 md:space-x-8">
+        <Link
+          href="/"
+          className="text-purple hover:text-orange tracking-wide md:tracking-widest text-base font-normal"
+        >
+          Home
+        </Link>
+        <Link
+          href="/projects"
+          className="text-purple hover:text-orange tracking-wide md:tracking-widest text-base font-normal"
+        >
+          Projects
+        </Link>
+        <Link
+          href="/about"
+          className="text-purple hover:text-orange tracking-wide md:tracking-widest text-base font-normal"
+        >
+          About
+        </Link>
+        <Link
+          href="/contact"
+          className="text-purple hover:text-orange tracking-wide md:tracking-widest text-base font-normal"
+        >
+          Contact
+        </Link>
+      </div>
     </nav>
   );
 };
