e6e714c37935ff3c65dc6bb104e87fe44d8de240
@piotr.blaz <piotr.blazejak@yahoo.com>
2023-10-13 22:28:15 +0200

    feat(project): created project page

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,14 +1,20 @@
+"use client";
 import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { usePathname } from "next/navigation";
 
-export default async function Template({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function Template({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
   return (
-    <div className=" min-h-screen w-full overflow-x-hidden bg-gradient-to-tl  from-light-navy to-navy">
+    <div
+      className={`min-h-screen w-full overflow-x-hidden bg-gradient-to-tl  ${
+        pathname.startsWith("/projects/")
+          ? "from-light-blue to-blue"
+          : "from-light-navy to-navy"
+      }`}
+    >
       <Navbar />
       <main>{children}</main>
       <Footer />
